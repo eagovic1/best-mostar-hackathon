f608c5f6d663d73dafc4b8b58576774d4f619068
eagovic1 <eagovic1@etf.unsa.ba>
2024-03-09 03:00:18 +0100

    add methods for notfying via mai

--- index.js
+++ index.js
@@ -12,11 +12,27 @@ const app = express();
 
 app.use(express.static('public'));
 
+const { sendMail } = require('./mail');
+
+function notifyTeacher(teacherMail, userName, bookName) {
+    let from = "emir.agovic13@gmail.com";
+    let to = teacherMail;
+    let subject = "New book request";
+    sendMail(from, to, subject, `Student ${userName} has made a request for the book ${bookName}!`);
+}
+
+function notifyStudent(studentMail, bookName, status) {
+    let from = "emir.agovic13@gmail.com";
+    let to = studentMail;
+    let subject = "New book request";
+    sendMail(from, to, subject, `Teacher has changed the status of your request for the book ${bookName} to ${status}!`);
+}
+
 initialize().then(() => {
     app.listen(3000, () => {
         console.log('Server is running on port 3000');
     });
-}); 
+});
 
 
 
