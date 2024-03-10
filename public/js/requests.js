3dd2ff3712172b50d2c4a74f899611443a3605ed
eagovic1 <eagovic1@etf.unsa.ba>
2024-03-10 01:01:34 +0100

    implement teacher module for requests

--- public/js/requests.js
+++ public/js/requests.js
@@ -1,3 +1,24 @@
+function requestStatusChange(requestId, status) {
+    console.log(requestId, status);
+    fetch("/request/book/status", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            requestId: requestId,
+            status: status,
+        }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            if (data.success) {
+                location.reload();
+            }
+        });
+}
+
 fetch("/login", {
     method: "POST",
     headers: {
@@ -24,10 +45,12 @@ fetch("/login", {
                         const row = document.createElement("tr");
                         row.innerHTML = `
                 <tr>
-                    <td>Jane Smith</td>
+                    <td>${_request.name}</td>
                     <td>${_request.title}</td>
-                    <td><button type="button" class="btn btn-success">Approve</button></td>
-                    <td><button type="button" class="btn btn-danger">Reject</button></td>
+                    <td>${_request.date}</td>
+                    <td>${(Boolean)(_request.graded)}</td>
+                    <td><button onclick="requestStatusChange('${_request.id}', 'approved')" type="button" class="btn btn-success">Approve</button></td>
+                    <td><button onclick="requestStatusChange('${_request.id}', 'rejected')" type="button" class="btn btn-danger">Reject</button></td>
                 </tr>
                     `;
                         document.getElementById("pending-requests").appendChild(row);
@@ -35,9 +58,41 @@ fetch("/login", {
                 } else {
                     const row = document.createElement("tr");
                     row.innerHTML = `
-                <td colspan="4">No pending requests</td>
+                <td colspan="6">No pending requests</td>
                 `;
                     document.getElementById("pending-requests").appendChild(row);
+                }
+            });
+
+        fetch("/history/approved", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                if (data.length > 0) {
+                    data.forEach((_request) => {
+                        const row = document.createElement("tr");
+                        row.innerHTML = `
+                <tr>
+                    <td>${_request.name}</td>
+                    <td>${_request.title}</td>
+                    <td>${_request.date}</td>
+                    <td>${(Boolean)(_request.graded)}</td>
+                    <td><button type="button" class="btn btn-info">Details</button></td>
+                </tr>
+                    `;
+                        document.getElementById("approved-requests").appendChild(row);
+                    });
+                } else {
+                    const row = document.createElement("tr");
+                    row.innerHTML = `
+                <td colspan="5">No approved requests</td>
+                `;
+                    document.getElementById("approved-requests").appendChild(row);
                 }
             });
 
